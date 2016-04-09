0d85c14bd76eaaff15a6afa88222d79c40925390
佐藤健太郎 <kanazaiku@yahoo.co.jp>
2016-04-09 23:11:48 +0900

    テスト更新

--- test/test/test.cs
+++ test/test/test.cs
@@ -8,17 +8,46 @@ namespace test
 	{
 		public App ()
 		{
+			var button = new Button{ 
+				Text = "Enter"
+			};
+			var datePicker = new DatePicker{ };
+
 			// The root page of your application
 			MainPage = new ContentPage {
 				Content = new StackLayout {
+					HorizontalOptions = LayoutOptions.Center,
 					VerticalOptions = LayoutOptions.Center,
 					Children = {
 						new Label {
 							XAlign = TextAlignment.Center,
 							Text = "Welcome to Xamarin Forms!"
-						}
+						},
+						datePicker,
+						button
+
 					}
+
+
 				}
+			};
+
+			button.Clicked += (sender, e) => {
+
+				MainPage = App.getSecondPage(datePicker.Date);
+			};
+				
+		}
+
+		public static ContentPage getSecondPage(DateTime selectDate){
+		
+			return new ContentPage {
+				Content = new Label{
+					Text = selectDate.ToString(),
+					VerticalOptions = LayoutOptions.CenterAndExpand,
+					HorizontalOptions = LayoutOptions.CenterAndExpand
+				},
+				BackgroundColor = Color.Yellow
 			};
 		}
 
